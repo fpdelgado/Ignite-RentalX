e712d20d2565bf4709097f915ded76807cb45551
Felipe <felipe.portillo.delgado@gmail.com>
2021-05-30 18:22:15 -0300

    finalizacao

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayLoad {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -20,7 +25,7 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayLoad;
     const user_id = sub;
 
@@ -50,7 +55,15 @@ class RefreshTokenUseCase {
       user_id,
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return {
+      token: newToken,
+      refresh_token,
+    };
   }
 }
 
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUser/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 import uploadConfig from '../../../../config/upload';
@@ -9,10 +10,11 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const userRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 userRoutes.post('/', createUserController.handle);
 
@@ -22,5 +24,7 @@ userRoutes.patch(
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle
 );
+
+userRoutes.get('/profile', ensureAuthenticated, profileUserController.handle);
 
 export { userRoutes };
